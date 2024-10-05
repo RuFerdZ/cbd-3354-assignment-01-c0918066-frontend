715b54bc777895775a174de644cdfc32d1bce294
Rusiru Fernando <37113657+RuFerdZ@users.noreply.github.com>
2024-10-05 17:59:38 -0400

    fix: migrated all user related functions to User Management component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import Home from './components/Home';
 import UploadFile from './components/UploadFile';
-import AddUser from './components/AddUser';
-import GetUser from './components/GetUser';
+import UserManagement from './components/UserManagement';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme();
@@ -20,15 +19,13 @@ const App = () => {
                         </Typography>
                         <Button color="inherit" component={Link} to="/">Home</Button>
                         <Button color="inherit" component={Link} to="/upload">Upload File</Button>
-                        <Button color="inherit" component={Link} to="/add-user">Add User</Button>
-                        <Button color="inherit" component={Link} to="/get-user">Get User</Button>
+                        <Button color="inherit" component={Link} to="/user">User Management</Button>
                     </Toolbar>
                 </AppBar>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/upload" element={<UploadFile />} />
-                    <Route path="/add-user" element={<AddUser />} />
-                    <Route path="/get-user" element={<GetUser />} />
+                    <Route path="/user" element={<UserManagement />} />
                 </Routes>
             </Router>
         </ThemeProvider>
